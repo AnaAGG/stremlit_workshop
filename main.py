0b3cbef25bd4f05302d2cfe3b2764fa457c5bb1f
Ana García <75084991+AnaAGG@users.noreply.github.com>
2021-06-17 14:52:00 +0200

    hasta mapa

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 
-from src.support import species_list
+from src.support import species_list, load_data, load_info, maps
 
 # to write a title in our streamlit page we can use the magic command `write`
 st.write ("""
@@ -23,20 +23,44 @@ with col2:
     st.image(imagen, use_column_width=True)
 
 
-# Insert images in streamlit, using one column
-#imagen = Image.open("Images/portada1.jpg")
-#st.image(imagen, use_column_width=True)
-
+#now 
 st.write(""" 
 ## List of species that you could find in this page""")
 
 x_options = species_list()
 
-col1, col2, col3 = st.beta_columns(3)
-with col1:
-    st.table(x_options[:5])
-with col2:
-    st.table(x_options[5:10])
-
+col3, col4, col5, col6 = st.beta_columns(4)
 with col3:
+    st.table(x_options[:5])
+with col4:
+    st.table(x_options[5:10])
+with col5:
     st.table(x_options[10:15])
+with col6:
+    st.table(x_options[15:20])
+
+
+
+#Now we have report the basic information for a given species
+
+st.header("Which species would you like to see?")
+x_axis = st.text_input("Buscador:")
+
+if len(x_axis) == 0:    
+        st.write("We need that you pass a species")
+else:
+    #load coordinates information
+    data = load_data()
+    data2 = data[data["species"] == f'{x_axis}']
+    st.dataframe(data2)
+
+    #load species info
+    z = load_info()
+    z.reset_index(inplace = True)
+    t = z[z["species"]== f"{x_axis}"]
+    t.drop(["species"], axis = 1, inplace = True)
+    st.table(t)
+
+    # create a map with the species presences
+    maps(data2)
+
--- src/support.py
+++ src/support.py
@@ -1,14 +1,50 @@
 import pandas as pd
+import folium
+from streamlit_folium import folium_static
+from folium import Choropleth, Circle, Marker, Icon, Map
 
 # Function to load the macroalgae data
 def load_data():
-    return pd.read_csv("data/macroalgae_final.csv")
+    return pd.read_csv("data/macroalgae_final.csv", index_col=0)
 
 # Function to load the macroalgae info
 def load_info():
-    return pd.read_csv("data/species_info.csv")
+    return pd.read_csv("data/species_info.csv", index_col=0)
 
 # Function to load create a list with all the species that we have in the dataset
 def species_list():
     data = load_data()
-    return tuple(data["species"].unique())
+    return list(data["species"].unique())
+
+def maps(df):
+    #showing the maps
+    map_sby = folium.Map(tiles="OpenStreetMap", location=[40.4146, -3.7004], zoom_start=2)
+    #design for the app
+
+    icono = Icon(color = "blue",
+                    prefix = "fa",
+                    icon = "home",
+                    icon_color = "black"
+        )
+
+    loc = {"location":[40.4146, -3.7004],
+                "tooltip": "Mi ubicación"}
+        
+    marker_ = Marker(**loc, icon = icono).add_to(map_sby)
+
+    for i,row in df.iterrows():
+                
+        icon = Icon(color = "green",
+                    prefix = "fa",
+                    icon = "fa-map-marker",
+                    icon_color = "black"
+        )
+
+        location_ = {"location" : [row["lat"],row["lon"]],
+                     "tooltip" : row["locality"]}
+                
+       
+
+        marker_2 = Marker(**location_, icon = icon).add_to(map_sby)
+
+    return folium_static(map_sby)
