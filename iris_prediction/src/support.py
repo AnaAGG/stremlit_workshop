ee95cdba0a8a7832011edf8c2b6c34a1de3b813c
Ana García <75084991+AnaAGG@users.noreply.github.com>
2021-06-19 22:09:14 +0200

    images

--- iris_prediction/src/support.py
+++ iris_prediction/src/support.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
 
 def load_data():
      data =  datasets.load_iris()
@@ -21,4 +22,25 @@ def user_input_features():
             'petal_width': petal_width}
 
     return pd.DataFrame(data, index=[0])
+
+def model():
+    iris = datasets.load_iris()
+    X = iris.data
+    Y = iris.target
+
+clf = RandomForestClassifier()
+clf.fit(X, Y)
+
+prediction = clf.predict(df)
+prediction_proba = clf.predict_proba(df)
+
+st.subheader('Class labels and their corresponding index number')
+st.write(iris.target_names)
+
+st.subheader('Prediction')
+st.write(iris.target_names[prediction])
+#st.write(prediction)
+
+st.subheader('Prediction Probability')
+st.write(prediction_proba)
 
