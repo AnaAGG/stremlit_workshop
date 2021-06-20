e47068dffdd124a190db58d1753d87662815c432
Ana García <75084991+AnaAGG@users.noreply.github.com>
2021-06-20 18:40:53 +0200

    docstrings

--- iris_prediction/src/support.py
+++ iris_prediction/src/support.py
@@ -4,15 +4,28 @@ from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
 
 def load_data():
+    """
+    Function to load the iris data
+    Args: 
+        non receive parameters
+    Returns:
+        The dataset with the species info
+    """
     
-     data =  datasets.load_iris()
-     df = pd.DataFrame(data.data, columns = data.feature_names)
+    data =  datasets.load_iris()
+    df = pd.DataFrame(data.data, columns = data.feature_names)
+    df2 = pd.DataFrame(data.target, columns = ["target"])
 
-     df2 = pd.DataFrame(data.target, columns = ["target"])
-
-     return df.join(df2)
+    return df.join(df2)
 
 def user_input_features():
+    """
+    Function to save the info that the user give in the app
+    Args: 
+        non receive parameters
+    Returns:
+        A dataframe with the characteristcs given by the user
+    """
     sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4) # the sidebar.slider magic function receive the max, min and default value in out sidebar
     sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
     petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
@@ -27,6 +40,16 @@ def user_input_features():
 
 
 def model(df,x,y):
+    """
+    Function fit a Random Forest Model
+    Args: 
+        df: complete dataframe with all the data
+        x: the predictors
+        y: the response variable (kind of species)
+    Returns:
+        pred: which species did you see in the field
+        pred_proba: the probability of such a prediction
+    """
     clf = RandomForestClassifier()
     clf.fit(x, y)
     
